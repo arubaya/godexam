4e82cef78ac31fe5dedc6ded0bb477baea5f55cb
arubaya <abi.arubaya@gmail.com>
2024-01-15 14:30:41 +0700

    add my exam list

--- app/(MainLayout)/my-exam/page.tsx
+++ app/(MainLayout)/my-exam/page.tsx
@@ -1,11 +1,45 @@
-import { Box } from "@mui/material";
-import React from "react";
+"use client";
+
+import ExamItem from "@/components/molecules/ExamItem";
+import { DUMMY_EXAM_DATA } from "@/constants/exams";
+import { Box, TextField } from "@mui/material";
+import React, { useMemo, useState } from "react";
 
 const MyExamPage = () => {
+  const [searchText, setSearchText] = useState("");
+  const processedExamList = useMemo(
+    () =>
+      DUMMY_EXAM_DATA?.filter((exam) =>
+        searchText !== ""
+          ? exam.title.toLowerCase().includes(searchText.toLowerCase())
+          : true
+      ),
+    [searchText]
+  );
   return (
-    <Box>
-      <Box>Exam in Progress</Box>
-      <Box>{`Today's Exam`}</Box>
+    <Box className="flex flex-col w-full gap-3">
+      <TextField
+        label="Search"
+        value={searchText}
+        margin="normal"
+        size="small"
+        sx={{ maxWidth: "300px" }}
+        autoComplete="off"
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <Box className="grid grid-cols-[repeat(auto-fit,_minmax(340px,_1fr))] gap-3">
+        {processedExamList.map((exam) => (
+          <ExamItem
+            key={exam.id}
+            id={exam.id}
+            title={exam.title}
+            examType={exam.examType}
+            duration={exam.duration}
+            date={exam.date}
+            totalQuestion={exam.totalQuestion}
+          />
+        ))}
+      </Box>
     </Box>
   );
 };
